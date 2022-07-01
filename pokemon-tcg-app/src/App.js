7b9367e505e72535cdabad0a1a565ef2834f7729
Matthieu Fernandez <clockwork_mango@hotmail.com>
2022-07-01 17:00:09 -0400

    added random deck generation

--- pokemon-tcg-app/src/App.js
+++ pokemon-tcg-app/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const randomPage = Math.floor(Math.random() * 60 + 1);
+
+  function randomCard() {
+    return Math.floor(Math.random() * 250 + 1);
+  }
+
+  // const [cards, setCards] = useState(null);
+
+  const [cards, setCards] = useState();
+  const [deck, setDeck] = useState([]);
+
+  useEffect(() => {
+    async function getCards() {
+      try {
+        await fetch(`https://api.pokemontcg.io/v2/cards/?page=${randomPage}`, {
+          method: "GET",
+          headers: {
+            "content-type": "application/json",
+            "X-Api-Key": API_KEY,
+          },
+        })
+          .then((res) => res.json())
+          .then((data) => setCards(data.data));
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    getCards();
+  }, []);
+
+  useEffect(() => {
+    if (cards) {
+      while (deck.length < 60) {
+        let check = false;
+        let insertCardNum = randomCard();
+        let insertCard = cards[insertCardNum];
+
+        deck.map((card) => {
+          if (card.id === insertCard.id) {
+            check = true;
+          }
+        });
+
+        if (!check) {
+          deck.push(insertCard);
+        }
+      }
+
+      console.log(deck);
+    }
+  }, [cards]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>App</h1>
     </div>
   );
 }
-
-export default App;
